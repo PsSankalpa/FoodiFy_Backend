7277e4077e5aeabcffbf67ff15bb9d3b215cbfed
Sankalpa1252 <62177348+PsSankalpa@users.noreply.github.com>
2022-09-16 18:29:11 +0530

    Update FoodCommentController.java

--- src/main/java/Foodify/Backend/controller/FoodCommentController.java
+++ src/main/java/Foodify/Backend/controller/FoodCommentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import Foodify.Backend.model.FoodComment;
 import Foodify.Backend.repository.FoodCommentRepository;
 
-@CrossOrigin(origins = "http://localhost:3000")
+
 @RestController
 public class FoodCommentController {
 	
